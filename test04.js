e75628cfa4d9a12065e4b3994a4b5e24b5751b2c
srawz <565473559@qq.com>
2017-02-25 12:04:06 +0800

    no message

--- test04.js
+++ test04.js
@@ -1,11 +1,33 @@
-var url = 'http://www.baidu.com';
 var page = require('webpage').create();
-page.onConsoleMessage = function (msg) {
-    console.log(msg);
-};
-page.open(url, function (status) {
-    page.evaluate(function () {
-    console.info(JSON.stringify(page.cookies))
-    });
-    phantom.exit();
-});
+var keyWord = 'ife';
+var url = 'http://www.baidu.com/s?wd=' + encodeURIComponent(keyWord);
+var beginTime = Date.now();
+page.open(url, function(e) {
+
+	page.includeJs("http://libs.baidu.com/jquery/1.9.1/jquery.min.js", function() {
+		var result = page.evaluate(function() {
+			var result = {
+				code: 1,
+				msg: '抓取成功',
+        time: '',
+        word: '',
+				dataList: []
+			}
+			var res = $('.result');
+			res.each(function() {
+				var imgs = $(this).find('.c-img')
+				result.dataList.push({
+					title: $(this).children('h3').text(),
+					info: $(this).find('.c-abstract').text(),
+					link: $(this).find('h3 a')[0].href,
+					pic: imgs.length > 0 ? imgs[0].src : ''
+				})
+			})
+			return result
+		})
+		result.time = Date.now() - beginTime;
+		result.word = keyWord;
+		console.log(JSON.stringify(result))
+		phantom.exit()
+	})
+})
