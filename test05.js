7cba5b67b38516c73eec939028e6bd5d0bfd7bd8
srawz <565473559@qq.com>
2017-02-25 14:42:08 +0800

    no message

--- test05.js
+++ test05.js
@@ -1,10 +1,34 @@
+
+var system = require('system');
 var page = require('webpage').create();
 var keyWord = 'ife';
 var url = 'http://www.baidu.com/s?wd=' + encodeURIComponent(keyWord);
 var beginTime = Date.now();
+var dev = {
+	ipad : {
+		ua : 'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+		width : 768,
+		height : 1024
+	},
+	iphone6 : {
+		ua : 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+		width : 375,
+		height : 667
+	},
+	iphone5 : {
+		ua : 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+		width : 320,
+		height : 568
+	}
+};
+var minput = system.args[1];
+page.settings.userAgent = dev[minput].ua;
+page.viewportSize = { width: dev[minput].width, height: dev[minput].height };
 page.open(url, function(e) {
 
+
 	page.includeJs("http://libs.baidu.com/jquery/1.9.1/jquery.min.js", function() {
+
 		var result = page.evaluate(function() {
 			var result = {
 				code: 1,
